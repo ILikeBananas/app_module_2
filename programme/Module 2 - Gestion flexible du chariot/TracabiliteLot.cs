5e78559ac0aba69c892b2f37ffefed6bb3d84ea1
Bilal Johry <bilal.johry@divtec.ch>
2019-09-08 20:59:44 +0200

    Création de nouvelles pages

---
+++ programme/Module 2 - Gestion flexible du chariot/TracabiliteLot.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Module_2___Gestion_flexible_du_chariot
+{
+    public partial class TracabiliteLot : Form
+    {
+        public TracabiliteLot()
+        {
+            InitializeComponent();
+        }
+    }
+}
