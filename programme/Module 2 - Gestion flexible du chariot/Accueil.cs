f268d40ce70e10ff16347786fb557fafa5ba3960
Jonny Hofmann <jonnyhofmann@gmx.ch>
2019-10-25 11:01:31 +0200

    Nettoyage "Acceuil"
    Fonction pour changer de form

--- programme/Module 2 - Gestion flexible du chariot/Accueil.cs
+++ programme/Module 2 - Gestion flexible du chariot/Accueil.cs
@@ -24,33 +24,33 @@ namespace Module_2___Gestion_flexible_du_chariot
 
         private void AllerEditionRecettes_Click(object sender, EventArgs e)
         {
-            Form frm = new EditionRecette();
-            frm.Location = this.Location;
-            frm.StartPosition = FormStartPosition.Manual;
-            frm.FormClosing += delegate { this.Show(); };
-            frm.Show();
-            this.Hide();
+            GoToForm(new EditionRecette());
 
         }
 
         private void AllerEditionLots_Click(object sender, EventArgs e)
         {
-            Form frm = new EditionLots();
-            frm.Location = this.Location;
-            frm.StartPosition = FormStartPosition.Manual;
-            frm.FormClosing += delegate { this.Show(); };
-            frm.Show();
-            this.Hide();
+            GoToForm(new EditionLots());
         }
 
         private void AllerTracabiliteLots_Click(object sender, EventArgs e)
         {
-
+            
         }
 
         private void AllerEvenements_Click(object sender, EventArgs e)
         {
 
+        }
+
+        // Changes the view to the new form
+        // This function is used to not repeat the same code for every button
+        private void GoToForm(Form frm) {
+            frm.Location = this.Location;
+            frm.StartPosition = FormStartPosition.Manual;
+            frm.FormClosing += delegate { this.Show(); };
+            frm.Show();
+            this.Hide();
         }
     }
 }
